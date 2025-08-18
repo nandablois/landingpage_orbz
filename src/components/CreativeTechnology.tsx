c8c6f1a1591c9ea1220c31ffad8412b34ebbcab6
Nanda Blois <nandablois1@gmail.com>
2025-08-18 14:24:32 -0300

    feat: Adiciona seção CreativeTechnology com ícones, títulos e layout responsivo e integra ao App

---
+++ src/components/CreativeTechnology.tsx
@@ -0,0 +1,39 @@
+import Sdk from './ui/svg/sdk'
+import Brush from './ui/svg/brush'
+import Chess from './ui/svg/chess'
+import Atr from './ui/svg/atr'
+
+const features = [
+  { icon: <Sdk />, title: "Projetos únicos" },
+  { icon: <Brush />, title: "Criatividade" },
+  { icon: <Chess />, title: "Estratégia" },
+  { icon: <Atr />, title: "Colaboração" },
+]
+
+export default function CreativeTechnology() {
+  return (
+    <section className="flex flex-col px-6 py-10 md:py-32 lg:py-40 text-very-dark-blue-purple bg-gray-light overflow-x-hidden">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between px-4 md:px-0 gap-12 md:gap-[120px]">
+
+        <div className="md:w-1/2 min-w-0 flex flex-col items-start gap-4 order-1 md:order-2">
+          <h1 className="font-black text-[32px] md:text-[50px] leading-tight md:mr-0 lg:mr-[380px]">
+            Na Orbz a criatividade encontra a tecnologia
+          </h1>
+          <p className="font-misto text-[16px] md:text-xl text-muted-gray md:mr-[240px] text-left md:text-left">
+            Somos uma <strong>equipe apaixonada por transformar</strong> conceitos em experiências digitais inesquecíveis. <strong>Combinando design criativo, desenvolvimento habilidoso e estratégia digital inteligente</strong>, estamos aqui para transformar suas ideias em realidade digital.
+          </p>
+        </div>
+
+        <div className="md:w-1/2 min-w-0 md:pl-[182px] grid grid-cols-2 gap-y-6 gap-x-4 text-muted-gray text-center md:text-left order-2 md:order-1">
+          {features.map((feature, index) => (
+            <div key={index} className="flex flex-col items-center md:items-start gap-2 max-w-[150px]">
+              <div className="mb-2 w-10 h-10 sm:w-10 sm:h-10 transform scale-75 sm:scale-100">{feature.icon}</div>
+              <span className="text-2xl font-semibold">{feature.title}</span>
+            </div>
+          ))}
+        </div>
+
+      </div>
+    </section>
+  )
+}
